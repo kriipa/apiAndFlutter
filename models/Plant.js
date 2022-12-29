29e87ea24f8e3f2f6c1783adbb4c0e8f2af2e147
shubham sapkota <shubham.sapkota1@gmail.com>
2022-12-29 18:09:51 +0545

    plant.js added

---
+++ models/Plant.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+const reviewSchema = mongoose.Schema({
+    body: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    reviewer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+})
+// One-to-Few is most suitable for embedding!
+const plantSchema = mongoose.Schema({
+    author: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    reviews: [reviewSchema],
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category'
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, { timestamps: true })
+
+module.exports = mongoose.model('Plant', plantSchema)
